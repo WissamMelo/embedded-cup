a090ee1cb3aa13c0a0a49f6b2ea4055ababd6c0c
Ícaro Pires <icaropsa@gmail.com>
2018-11-14 16:55:08 -0200

    Pass implementation to class

--- modules/network/Client/Client.ino
+++ modules/network/Client/Client.ino
@@ -1,8 +1,41 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+const char * ssid = "SSID";
+const char * password = "password";
 
-const char * ssid = "LadesConvidado";
-const char * password = "bemvindoconvidado";
+namespace smc {
+	class Client {
+		public:
+			Client(){
+				destHost = "http://host:port/create/";
+
+				http.begin(destHost); //Specify destination for HTTP request
+				http.addHeader("Content-Type", "application/json");
+			}
+			~Client(){
+				http.end();  //Free resources
+			}
+			int makeRequest(String payload){
+				int httpResponseCode = http.POST(payload); //Send the actual POST request
+
+				if(httpResponseCode > 0){
+					String response = http.getString();  //Get the response to the request
+					Serial.println(httpResponseCode);   //Print return code
+					Serial.println(response);           //Print request answer
+				}else{
+					Serial.print("Error on sending POST: ");
+					Serial.println(httpResponseCode);
+				}
+
+				return httpResponseCode;
+			}
+
+		private:
+			String destHost;
+
+			HTTPClient http;
+	};
+}
 
 void setup() {
 	Serial.begin(115200);
@@ -14,26 +47,11 @@ void setup() {
 		delay(1000);
 		Serial.println("Connecting to WiFi..");
 	}
-
 	Serial.println("Connected to the WiFi network");
 }
 
 void loop() {
-	HTTPClient http;
-	http.begin("http://172.16.1.105:9000/posts"); //Specify destination for HTTP request
-	http.addHeader("Content-Type", "application/json");
-	int httpResponseCode = http.POST("{\"slot\": \"10\", \"event\": \"nao tomou\"}"); //Send the actual POST request
-
-	if(httpResponseCode > 0){
-		String response = http.getString();  //Get the response to the request
-
-		Serial.println(httpResponseCode);   //Print return code
-		Serial.println(response);           //Print request answer
-	}else{
-		Serial.print("Error on sending POST: ");
-		Serial.println(httpResponseCode);
-
-	}
-	http.end();  //Free resources
-	delay(60*1000);  //Send a request every 10 seconds
+	smc::Client client;
+	client.makeRequest("{\"slot\": \"10\", \"event\": \"nao tomou\"}");
+	delay(60*1000);
 }
