c05e7cc56f754e6d1aac3f50f546b79cb3d61754
Lucas Mattioli <lucas.mattioli@live.com>
2018-11-21 17:18:10 -0200

    Update smc log.

--- modules/network/AccessPoint/AccessPoint.ino
+++ modules/network/AccessPoint/AccessPoint.ino
@@ -22,7 +22,7 @@ namespace smc {
             WiFiClient client = server.available();
             std::map<String, String> credentials;
             if(client) {
-                Serial.println("New client connected.");
+                Serial.println("[SMC-LOG] New client connected.");
                 String current_line = "";
                 String header = "";
                 while(client.connected()) {
@@ -51,7 +51,7 @@ namespace smc {
                 }
                 header = "";
                 client.stop();
-                Serial.println("Client disconnected.");
+                Serial.println("[SMC-LOG] Client disconnected.");
                 Serial.println("");
             }
             return credentials;
@@ -66,48 +66,48 @@ namespace smc {
             const static char* ssid = "SMC-AP";
             const static char* password = "123456789";
             if(WiFi.softAP(ssid, password)) {
-                Serial.print("Wifi soft AP started successfully. Access it in ");
+                Serial.print("[SMC-LOG] Wifi soft AP started successfully. Access it in ");
                 Serial.println(WiFi.softAPIP());
             }
             else {
-                Serial.println("Wifi soft AP start failed.");
+                Serial.println("[SMC-LOG] Wifi soft AP start failed.");
             }
             server.begin();
         }
 
         void end_soft_ap() {
             WiFi.softAPdisconnect(false);
-            Serial.println("Disconnecting Wifi soft AP...");
+            Serial.println("[SMC-LOG] Disconnecting Wifi soft AP...");
             delay(2000);
-            Serial.println("Wifi soft AP disconnected.");
+            Serial.println("[SMC-LOG] Wifi soft AP disconnected.");
             server.end();
-            Serial.println("Ending AP server...");
+            Serial.println("[SMC-LOG] Ending AP server...");
             delay(2000);
-            Serial.println("AP server ended.");
+            Serial.println("[SMC-LOG] AP server ended.");
         }
 
         void read_html_file() {
             if(SPIFFS.begin()) {
-                Serial.println("Card mounted successfully.");
+                Serial.println("[SMC-LOG] Card mounted successfully.");
             }
             else {
-                Serial.println("Card mount failed.");
+                Serial.println("[SMC-LOG] Card mount failed.");
             }
             File html_file = SPIFFS.open("/index.html");
             if(html_file) {
                 while(html_file.available()) {
                     html_page += char(html_file.read());
                 }
-                Serial.println("HTML page read successfully.");
+                Serial.println("[SMC-LOG] HTML page read successfully.");
                 html_file.close();
             }
             else {
-                Serial.println("Could not open index file.");
+                Serial.println("[SMC-LOG] Could not open index file.");
             }
         }
 
         std::map<String, String> handle_header(String header) const {
-            Serial.println("Handling header...");
+            Serial.println("[SMC-LOG] Handling header...");
             Serial.println(header);
 
             int ssid_index = std::max(header.indexOf("?SSID"), header.indexOf("&SSID"));
@@ -138,17 +138,15 @@ namespace smc {
     };
 }
 
-smc::AccessPoint ap;
-
 void handle_new_credentials(std::map<String, String> credentials) {
-    Serial.println("Handling new credentials...");
+    Serial.println("[SMC-LOG] Handling new credentials...");
     if(WiFi.status() == WL_CONNECTED) {
-        Serial.println("Already connected to an Wifi network...");
+        Serial.println("[SMC-LOG] Already connected to an Wifi network...");
         return;
     }
 
-    Serial.println("ssid: " + credentials["ssid"]);
-    Serial.println("password: " + credentials["password"]);
+    Serial.println("[SMC-LOG] ssid: " + credentials["ssid"]);
+    Serial.println("[SMC-LOG] password: " + credentials["password"]);
 
     // try to connect with new credentials on wifi here
     // in case of success, store credentials in wifi_credentials.txt file.
@@ -161,20 +159,22 @@ void handle_new_credentials(std::map<String, String> credentials) {
     while(WiFi.status() != WL_CONNECTED && tries < N_TRIES) {
         delay(1000);
         tries++;
-        Serial.print("Try #");
+        Serial.print("[SMC-LOG] Try #");
         Serial.print(tries);
         Serial.println(": connecting to wifi with new credentials...");
     }
 
     if(WiFi.status() == WL_CONNECTED) {
-        Serial.println("Connected successfully to wifi with new credentials.");
+        Serial.println("[SMC-LOG] Connected successfully to wifi with new credentials.");
         // record credentials in file here.
     }
     else {
-        Serial.println("Could not connect to wifi with new credentials.");
-        Serial.println("Starting AP again...");
+        Serial.println("[SMC-LOG] Could not connect to wifi with new credentials.");
+        Serial.println("[SMC-LOG] Starting AP again...");
     }
 }
+
+smc::AccessPoint ap;
 
 void setup() {
     Serial.begin(115200);
@@ -189,5 +189,5 @@ void loop() {
     if(!credentials.empty()) {
         handle_new_credentials(credentials);
     }
-    delay(100);
+    delay(0);
 }
