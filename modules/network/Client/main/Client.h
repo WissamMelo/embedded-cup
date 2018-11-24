9e77c058f5b8afdb1d0bec5b9697c8b815f8acb7
Ícaro Pires <icaropsa@gmail.com>
2018-11-24 12:23:48 -0200

    Fix indentation

--- modules/network/Client/main/Client.h
+++ modules/network/Client/main/Client.h
@@ -8,38 +8,38 @@ const char * ssid = "SSID";
 const char * password = "password";
 
 namespace smc {
-	class Client {
-		public:
-			Client(){
-				destHost = "http://host:port/create/";
+    class Client {
+        public:
+            Client(){
+                destHost = "http://host:port/create/";
 
-				http.begin(destHost);
-				http.addHeader("Content-Type", "application/json");
-			}
-			~Client(){
-				http.end();
-			}
-			int makeRequest(PayloadEncoder payload){
+                http.begin(destHost);
+                http.addHeader("Content-Type", "application/json");
+            }
+            ~Client(){
+                http.end();
+            }
+            int makeRequest(PayloadEncoder payload){
                 String payloadStr = payload.encode();
-				int httpResponseCode = http.POST(payloadStr);
+                int httpResponseCode = http.POST(payloadStr);
 
-				if(httpResponseCode > 0){
-					String response = http.getString();
-					Serial.println(httpResponseCode);
-					Serial.println(response);
-				}else{
-					Serial.print("[ERROR] POST request returned an error: ");
-					Serial.println(httpResponseCode);
-				}
+                if(httpResponseCode > 0){
+                    String response = http.getString();
+                    Serial.println(httpResponseCode);
+                    Serial.println(response);
+                }else{
+                    Serial.print("[ERROR] POST request returned an error: ");
+                    Serial.println(httpResponseCode);
+                }
 
-				return httpResponseCode;
-			}
+                return httpResponseCode;
+            }
 
-		private:
-			String destHost;
+        private:
+            String destHost;
 
-			HTTPClient http;
-	};
+            HTTPClient http;
+    };
 }
 
 #endif  // CLIENT_H_
--- modules/network/Client/main/main.ino
+++ modules/network/Client/main/main.ino
@@ -3,36 +3,36 @@
 #include "Client.h"
 
 void setup() {
-	Serial.begin(115200);
-	delay(4000);   //Delay needed before calling the WiFi.begin
+    Serial.begin(115200);
+    delay(4000);   //Delay needed before calling the WiFi.begin
 
-	WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
 
-	while (WiFi.status() != WL_CONNECTED) { //Check for the connection
-		delay(1000);
-		Serial.println("Connecting to WiFi..");
-	}
-	Serial.println("Connected to the WiFi network");
+    while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+        delay(1000);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
 }
 
 void loop() {
-	smc::Moment moment;
-	moment.hour = 10;
-	moment.minute = 20;
-	moment.day = 10;
-	moment.month = 10;
-	moment.year = 2018;
+    smc::Moment moment;
+    moment.hour = 10;
+    moment.minute = 20;
+    moment.day = 10;
+    moment.month = 10;
+    moment.year = 2018;
 
-	smc::AlarmInfo alarmInfo;
-	alarmInfo.start.hour = 10;
-	alarmInfo.start.minute = 13;
-	alarmInfo.period.hour = 11;
-	alarmInfo.period.minute = 12;
+    smc::AlarmInfo alarmInfo;
+    alarmInfo.start.hour = 10;
+    alarmInfo.start.minute = 13;
+    alarmInfo.period.hour = 11;
+    alarmInfo.period.minute = 12;
 
 
-	smc::Client client;
-	smc::PayloadEncoder payload(1, 2, moment, "registered", alarmInfo);
-	client.makeRequest(payload);
+    smc::Client client;
+    smc::PayloadEncoder payload(1, 2, moment, "registered", alarmInfo);
+    client.makeRequest(payload);
 
-	delay(20*1000);
+    delay(20*1000);
 }
